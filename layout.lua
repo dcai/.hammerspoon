f9108f2b34f3b8e66b8667a0a78c7a22af9a8c34
Dongsheng Cai <d@tux.im>
2017-05-17 16:17:48 +1000

    refactor layout.lua

--- layout.lua
+++ layout.lua
@@ -1,9 +1,9 @@
 --
 -- Layouts
 --
-local asusLCD = "ASUS VS247"
-local dellLCD = "DELL U2414H"
-local mainScreen = hs.screen.mainScreen()
+local screenAsusVs247 = "ASUS VS247"
+local screenDellU2414H = "DELL U2414H"
+local screenMain = hs.screen.mainScreen()
 
 local appChrome = "Google Chrome"
 local appSublime = "Sublime Text"
@@ -13,51 +13,47 @@ presetLayouts = {
     {
         name="Code",
         description="For fun and profit",
-        small={
-            {appiTerm, nil, mainScreen, positions.maximized, nil, nil},
-        },
-        large={
-            {appSublime, nil, mainScreen, positions.right50, nil, nil},
-            {appiTerm, nil, mainScreen, positions.right50, nil, nil},
+        definition={
+            {appiTerm, nil, screenMain, positions.maximized, nil, nil},
         }
     },
     {
         name="Write",
         description="Time to write",
-        small={
-            {appChrome, nil, mainScreen, positions.left50, nil, nil},
-            {appiTerm,   nil, mainScreen, positions.right50, nil, nil},
+        definition={
+            {appChrome, nil, screenMain, positions.left50, nil, nil},
+            {appiTerm,  nil, screenMain, positions.right50, nil, nil},
         }
     },
     {
-        name="DELL U2414H",
-        description="work",
-        small={
-            {appChrome, nil, mainScreen, positions.maximized, nil, nil},
-            {"Firefox", nil, dellLCD, positions.lower50, nil, nil},
-            {"Safari", nil, dellLCD, positions.lower60, nil, nil},
-            {"Slack", nil, dellLCD, positions.lower30, nil, nil},
-            {appiTerm, nil, dellLCD, positions.large, nil, nil},
-            {appSublime, nil, dellLCD, positions.lower40, nil, nil},
-            {"VLC", nil, dellLCD, positions.chat, nil, nil},
+        name="Work",
+        description="Lrn",
+        definition={
+            {appChrome,  nil, screenMain, positions.maximized, nil, nil},
+            {"Firefox",  nil, screenDellU2414H, positions.lower50, nil, nil},
+            {"Safari",   nil, screenDellU2414H, positions.lower60, nil, nil},
+            {"Slack",    nil, screenDellU2414H, positions.lower30, nil, nil},
+            {appiTerm,   nil, screenDellU2414H, positions.large, nil, nil},
+            {appSublime, nil, screenDellU2414H, positions.lower40, nil, nil},
+            {"VLC",      nil, screenDellU2414H, positions.chat, nil, nil},
         }
     },
     {
         name="Standup",
         description="Just Firefox",
-        small={
-            {"Firefox", nil, mainScreen, positions.maximized, nil, nil},
+        definition={
+            {"Firefox", nil, screenMain, positions.maximized, nil, nil},
         }
     },
 }
 function applyLayout(screen, layout)
-    local layoutSize = layout.small
+    local l = layout.definition
     if layout.large and screen:currentMode().w > 1500 then
-        layoutSize = layout.large
+        l = layout.large
     end
 
     currentLayout = layout
-    hs.layout.apply(layoutSize, function(windowTitle, layoutWindowTitle)
+    hs.layout.apply(l, function(windowTitle, layoutWindowTitle)
         return string.sub(windowTitle, 1, string.len(layoutWindowTitle)) == layoutWindowTitle
     end)
 end
@@ -68,7 +64,7 @@ function chooseLayout()
     local layoutChooser = hs.chooser.new(function(selection)
         if not selection then return end
 
-        applyLayout(mainScreen, presetLayouts[selection.index])
+        applyLayout(screenMain, presetLayouts[selection.index])
     end)
     i = 0
     layoutChooser:choices(hs.fnutils.imap(presetLayouts, function(layout)
@@ -89,21 +85,20 @@ function chooseLayout()
     end)
 end
 
-function applyLayoutToScreen()
+function applyDefaultLayout()
     if hostName == 'dsmbp15' then
-        local dellLCDLayout = presetLayouts[3].small
-        hs.layout.apply(dellLCDLayout)
-        --hs.layout.apply(asusLCDLayout)
+        local screenDellU2414HLayout = presetLayouts[3].definition
+        hs.layout.apply(screenDellU2414HLayout)
     end
 end
 
 chooseLayout();
-applyLayoutToScreen();
+applyDefaultLayout();
 
 hs.screen.watcher.new(function()
     -- hs.alert.show("Screen changed")
 
     --if not currentLayout then return end
     --  applyLayout(currentLayout)
-    applyLayoutToScreen();
+    applyDefaultLayout();
 end):start()
