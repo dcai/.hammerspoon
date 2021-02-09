390cdef8abdccf7b6035afc1a1b99968ef40c438
Dongsheng Cai <d@tux.im>
2021-02-09 20:41:17 +1100

    window management

--- grid.lua
+++ grid.lua
@@ -2,37 +2,57 @@
 -- Grid
 --
 
+local leftGrids = {positions.left50, positions.left66, positions.left34}
+local rightGrids = {positions.right50, positions.right66, positions.right34}
+local topGrids = {positions.upper50}
+local bottomGrids = {positions.lower50}
+
 grid = {
-    {key="u", units={positions.upper50Left50}},
-    {key="i", units={positions.upper50}},
-    {key="o", units={positions.upper50Right50}},
-    {key="j", units={positions.left50, positions.left66, positions.left34}},
-    {key="k", units={positions.centered, positions.maximized}},
-    {key="l", units={positions.right50, positions.right66, positions.right34}},
-    {key="m", units={positions.lower50Left50}},
-    {key=",", units={positions.lower50}},
-    {key=".", units={positions.chat}},
-    {key="return", units={positions.maximized}},
+    -- up
+    {key = "k", units = topGrids},
+    {key = "up", units = topGrids},
+    
+    -- down
+    {key = "down", units = bottomGrids},
+    {key = "j", units = bottomGrids},
+    
+    -- left
+    {key = "left", units = leftGrids},
+    {key = "l", units = rightGrids},
+    
+    -- right
+    {key = "h", units = leftGrids},
+    {key = "right", units = rightGrids},
+    
+    -- center and max
+    {key = "return", units = {positions.centered, positions.maximized}},
+    -- {key="return", units={positions.maximized}},
+    
+    -- others
+    {key = "o", units = {positions.upper50Right50}},
+    {key = "m", units = {positions.lower50Left50}},
+    {key = ".", units = {positions.chat}},
+    {key = "u", units = {positions.upper50Left50}},
 }
 
 hs.fnutils.each(grid, function(entry)
-
+    
     hs.hotkey.bind(keyGrid, entry.key, function()
-
+        
         local units = entry.units
         local screen = hs.screen.mainScreen()
         local window = hs.window.focusedWindow()
         local windowGeo = window:frame()
-
+        
         local index = 0
         hs.fnutils.find(units, function(unit)
             index = index + 1
-
+            
             local geo = hs.geometry.new(unit):fromUnitRect(screen:frame()):floor()
             return windowGeo:equals(geo)
         end)
         if index == #units then index = 0 end
-
+        
         currentLayout = null
         window:moveToUnit(units[index + 1])
     end)
--- init.lua
+++ init.lua
@@ -11,38 +11,37 @@ hostName = hs.host.localizedName()
 
 positions = {
     maximized = hs.layout.maximized,
-    centered = {x=0.15, y=0.15, w=0.7, h=0.7},
-    large = {x=0.01, y=0.02, w=0.98, h=0.96},
-
-    left34 = {x=0, y=0, w=0.34, h=1},
+    centered = {x = 0.15, y = 0.15, w = 0.7, h = 0.7},
+    large = {x = 0.01, y = 0.02, w = 0.98, h = 0.96},
+    
+    left34 = {x = 0, y = 0, w = 0.34, h = 1},
     left50 = hs.layout.left50,
-    left66 = {x=0, y=0, w=0.66, h=1},
+    left66 = {x = 0, y = 0, w = 0.66, h = 1},
     left70 = hs.layout.left70,
-
+    
     right30 = hs.layout.right30,
-    right34 = {x=0.66, y=0, w=0.34, h=1},
+    right34 = {x = 0.66, y = 0, w = 0.34, h = 1},
     right50 = hs.layout.right50,
-    right66 = {x=0.34, y=0, w=0.66, h=1},
-
-    upper30 = {x=0, y=0, w=1, h=0.3},
-    upper40 = {x=0, y=0, w=1, h=0.4},
-    upper50 = {x=0, y=0, w=1, h=0.5},
-    upper66 = {x=0, y=0, w=1, h=0.66},
-    upper50Left50 = {x=0, y=0, w=0.5, h=0.5},
-    upper50Right15 = {x=0.85, y=0, w=0.15, h=0.5},
-    upper50Right30 = {x=0.7, y=0, w=0.3, h=0.5},
-    upper50Right50 = {x=0.5, y=0, w=0.5, h=0.5},
-
-    lower30 = {x=0, y=0.7, w=1, h=0.3},
-    lower40 = {x=0, y=0.6, w=1, h=0.4},
-    lower50 = {x=0, y=0.5, w=1, h=0.5},
-    lower60 = {x=0, y=0.4, w=1, h=0.6},
-    lower70 = {x=0, y=0.3, w=1, h=0.7},
-
-    lower50Left50 = {x=0, y=0.5, w=0.5, h=0.5},
-    lower50Right50 = {x=0.5, y=0.5, w=0.5, h=0.5},
-    chat = {x=0.2, y=0.6, w=0.6, h=0.3}
-}
+    right66 = {x = 0.34, y = 0, w = 0.66, h = 1},
+    
+    upper30 = {x = 0, y = 0, w = 1, h = 0.3},
+    upper40 = {x = 0, y = 0, w = 1, h = 0.4},
+    upper50 = {x = 0, y = 0, w = 1, h = 0.5},
+    upper66 = {x = 0, y = 0, w = 1, h = 0.66},
+    upper50Left50 = {x = 0, y = 0, w = 0.5, h = 0.5},
+    upper50Right15 = {x = 0.85, y = 0, w = 0.15, h = 0.5},
+    upper50Right30 = {x = 0.7, y = 0, w = 0.3, h = 0.5},
+    upper50Right50 = {x = 0.5, y = 0, w = 0.5, h = 0.5},
+    
+    lower30 = {x = 0, y = 0.7, w = 1, h = 0.3},
+    lower40 = {x = 0, y = 0.6, w = 1, h = 0.4},
+    lower50 = {x = 0, y = 0.5, w = 1, h = 0.5},
+    lower60 = {x = 0, y = 0.4, w = 1, h = 0.6},
+    lower70 = {x = 0, y = 0.3, w = 1, h = 0.7},
+    
+    lower50Left50 = {x = 0, y = 0.5, w = 0.5, h = 0.5},
+    lower50Right50 = {x = 0.5, y = 0.5, w = 0.5, h = 0.5},
+chat = {x = 0.2, y = 0.6, w = 0.6, h = 0.3}}
 
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Hammerspoon reloaded @ " .. hostName)
