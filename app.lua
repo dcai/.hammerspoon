6858f4f64e0ac8bc10e59d3e91f670d2710c70e1
Dongsheng Cai <d@tux.im>
2021-02-21 20:30:57 +1100

    app watcher for finder

--- app.lua
+++ app.lua
@@ -38,3 +38,15 @@ bindAppKey("\\", "Dictionary.app")
 --     local rect = nextScreen:fullFrame()
 --     local center = hs.geometry.rectMidPoint(rect)hs.mouse.setAbsolutePosition(center)
 -- end)
+appFinderWatcher =
+    hs.application.watcher.new(
+    function(appName, eventType, appObject)
+        if (eventType == hs.application.watcher.activated) then
+            if (appName == "Finder") then
+                -- Bring all Finder windows forward when one gets activated
+                appObject:selectMenuItem({"Window", "Bring All to Front"})
+            end
+        end
+    end
+)
+appFinderWatcher:start()
